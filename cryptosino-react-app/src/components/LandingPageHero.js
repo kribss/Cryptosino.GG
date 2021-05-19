ee677aa2ab8c5fa27dd7e02c52c94ed7655e207e
Jonathan McDonald <jonnymcshu@gmail.com>
2021-05-18 20:24:24 -0700

    landing page setup

---
+++ cryptosino-react-app/src/components/LandingPageHero.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./LandingPageHero.css";
+
+function LandingPageHero() {
+    return (
+        <div className="landingpagehero-section">
+            <div className="landingpagehero-container">
+                <img className="landingpagehero-background" src="images/moneybackground.jpg"></img>
+                <h1>CryptoSino.GG</h1>
+            </div>
+        </div>
+    );
+}
+
+export default LandingPageHero;
---
+++ cryptosino-react-app/src/components/Pages/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LandingPageHero from "../LandingPageHero";
+
+function LandingPage() {
+    return (
+        <>
+            <LandingPageHero />
+        </>
+    )
+}
+
+export default LandingPage
